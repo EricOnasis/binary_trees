54fa30c4131965e4a13eec03e92791485d244f8b
Eric Onasis <ericonasis@gmail.com>
2023-11-02 01:36:51 +0300

    Fix Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,6 +8,7 @@
  *
  * Return: The height of the tree or 0 if tree is NULL.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -16,5 +17,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
