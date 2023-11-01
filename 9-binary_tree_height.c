6cc4f5345f41426631775abb86466fe8d1b33625
Eric Onasis <ericonasis@gmail.com>
2023-11-02 01:39:49 +0300

    Fix Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_left > height_right ? height_left : height_right);
 }
