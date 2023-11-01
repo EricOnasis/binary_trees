933e7ee479394df2f708bb18b79fff009ffc0336
Eric Onasis <ericonasis@gmail.com>
2023-11-02 02:02:39 +0300

    Fix Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,16 @@
  * Return: The balance factor of the binary tree.
  * If the tree is NULL, return 0.
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    int left_height, right_height;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+    if (tree == NULL)
+        return (0);
 
-	return ((int)(left_height - right_height));
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height - right_height);
 }
