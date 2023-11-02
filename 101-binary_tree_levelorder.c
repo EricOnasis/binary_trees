ee8257503b0d57f417af4463d89903b1bcd1ab0b
Eric Onasis <ericonasis@gmail.com>
2023-11-02 03:04:50 +0300

    Fix Task 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -8,40 +8,35 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-int binary_tree_is_complete(const binary_tree_t *tree)
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    binary_tree_t *queue[1024];
-    size_t front = 0, rear = 0;
-    int hasNullChild = 0;
+	binary_tree_t *queue[1024];
+	size_t front = 0, rear = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL || func == NULL)
+		return;
 
-    queue[rear] = (binary_tree_t *)tree;
+	queue[rear] = (binary_tree_t *)tree;
+	rear++;
 
-    rear++;
+	while (front < rear)
+	{
+		binary_tree_t *current = queue[front];
 
-    while (front < rear)
-    {
-        binary_tree_t *current = queue[front];
+		front++;
 
-        front++;
+		func(current->n);
 
-        if (current == NULL)
-        {
-            hasNullChild = 1;
-        }
-        else
-        {
-            if (hasNullChild)
-                return 0;
+		if (current->left)
+		{
+			queue[rear] = current->left;
+			rear++;
+		}
 
-            queue[rear] = current->left;
-            rear++;
-            queue[rear] = current->right;
-            rear++;
-        }
-    }
-
-    return (1);
+		if (current->right)
+		{
+			queue[rear] = current->right;
+			rear++;
+		}
+	}
 }
