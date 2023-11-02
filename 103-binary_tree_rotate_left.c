5ba88f3e51273167ad53efc6a29ffd97367596c4
Eric Onasis <ericonasis@gmail.com>
2023-11-02 03:29:22 +0300

    Third Fix

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,27 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root = tree->right;
+	if (tree == NULL || tree->right == NULL)
+		return (tree);
 
-    if (tree == NULL || tree->right == NULL)
-        return (tree);
+	binary_tree_t *new_root = tree->right;
 
-    tree->right = new_root->left;
+	tree->right = new_root->left;
 
-    if (new_root->left != NULL)
-        new_root->left->parent = tree;
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
 
-    new_root->left = tree;
-    new_root->parent = tree->parent;
-    tree->parent = new_root;
+	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
 
-    if (new_root->parent != NULL)
-    {
-        if (new_root->parent->left == tree)
-            new_root->parent->left = new_root;
-        else
-            new_root->parent->right = new_root;
-    }
+	if (new_root->parent != NULL)
+	{
+		if (new_root->parent->left == tree)
+			new_root->parent->left = new_root;
+		else
+			new_root->parent->right = new_root;
+	}
 
-    return (new_root);
+	return (new_root);
 }
